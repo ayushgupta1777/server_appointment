2d02f1d4c0d6db2a38fe424af8c5bf03cde20579
ayush gupta <ayushgupta1733@gmail.com>
2024-07-29 23:48:41 +0530

    second commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:3000', // React app's origin
+    origin: 'https://appointmenttech0.web.app', // React app's origin
     credentials: true, // If you need to send cookies with the requests
   }));
   
